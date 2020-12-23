1538236ab5a1ca22c86e6c9fd561deb4750e7bdd
Bill <dariennx@hotmail.com>
2020-12-23 18:10:59 -0300

    starting portfolio

---
+++ FromZeroToHero/portfolio/script.js
@@ -0,0 +1,29 @@
+/*header menu*/
+let coll = document.getElementsByClassName("collapsible");
+
+
+for (let i = 0; i < coll.length; i++) {
+  coll[i].addEventListener("click", function() {
+    this.classList.toggle("active");
+    let content = this.nextElementSibling;
+    if (content.style.position === "block") {
+      content.style.display = "none";
+    } else {
+      content.style.display = "block";
+    }
+  });
+}
+/*start of collapsible animation */ 
+for (i = 0; i < coll.length; i++) {
+    coll[i].addEventListener("click", function() {
+      this.classList.toggle("active");
+      var content = this.nextElementSibling;
+      if (content.style.maxHeight){
+        content.style.maxHeight = null;
+      } else {
+        content.style.maxHeight = content.scrollHeight + "px";
+      }
+    });
+  }
+  /*end of collapsible function */ 
+/*end of header menu*/
