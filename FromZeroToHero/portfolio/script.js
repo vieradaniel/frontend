114290735b35f9854b44f2eae27d0321ec4cbf3b
Bill <dariennx@hotmail.com>
2020-12-25 03:34:51 -0300

    logo truchi

--- FromZeroToHero/portfolio/script.js
+++ FromZeroToHero/portfolio/script.js
@@ -17,7 +17,7 @@ for (let i = 0; i < coll.length; i++) {
 for (let i = 0; i < coll.length; i++) {
     coll[i].addEventListener("click", function() {
       this.classList.toggle("active");
-      var content = this.nextElementSibling;
+      let content = this.nextElementSibling;
       if (content.style.maxHeight){
         content.style.maxHeight = null;
       } else {
